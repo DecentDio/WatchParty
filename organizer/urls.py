3a18f0c0a0d2309e283891d12fd7d1a66ce09688
gs8ckb <gs8ckb@virginia.edu>
2022-03-25 00:18:54 -0400

    My Watchparties Link
    added hyperlink to my watchparties page from login page

--- organizer/urls.py
+++ organizer/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'organizer'
 urlpatterns = [
     path('', views.login, name='login'),
     path('watchparties', views.WatchParties.as_view(), name='watchparties'),
