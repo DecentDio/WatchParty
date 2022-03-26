b0867e4a6747e8dd605e936da93b26f77691be16
DecentDio <diosulimana@gmail.com>
2022-03-25 20:03:49 -0400

    Working? feature

--- organizer/models.py
+++ organizer/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Watchparty(models.Model):
     account = models.ForeignKey(User, on_delete=models.CASCADE)
     title_text = models.CharField(max_length=200)
     start_date = models.DateField('date range start')
     end_date = models.DateField('date range end')
+
     def __str__(self):
         return self.title_text
+
 
 class AddedUser(models.Model):
     account = models.ForeignKey(User, on_delete=models.CASCADE)
     watchparty = models.ForeignKey(Watchparty, on_delete=models.CASCADE)
+
 
 class AvailabilityRange(models.Model):
     account = models.ForeignKey(User, on_delete=models.CASCADE)
--- organizer/urls.py
+++ organizer/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'organizer'
 urlpatterns = [
     path('', views.login, name='login'),
+    path("makeParty", views.GetParty, name="form"),
+    path("addUsers", views.GetAdded, name="users"),
+    path("addAvil", views.GetAvil, name="avil"),
     path('watchparties', views.WatchParties.as_view(), name='watchparties'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
