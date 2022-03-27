7769b936307880e5a4a935b4b5613690ea43b36e
gs8ckb <gs8ckb@virginia.edu>
2022-03-27 17:58:39 -0400

    valid_time_range
    added valid_time_range method to availabilityrange model

--- organizer/tests.py
+++ organizer/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Watchparty
+from .models import Watchparty, AvailabilityRange
 from django.contrib.auth.models import User
 
 class WatchpartyModelTests(TestCase):
@@ -17,5 +17,28 @@ class WatchpartyModelTests(TestCase):
         my_user = User.objects.create(username='Testuser')
         test_watchparty = Watchparty(account=my_user,title_text='test watch party', start_date='2022-02-22', end_date='2022-02-22')
         self.assertIs(test_watchparty.valid_date_range(), True)
+
+class AvailabilityRangeModelTests(TestCase):
+    def test_valid_time_range(self):
+        my_user = User.objects.create(username='Testuser')
+        test_watchparty = Watchparty(account=my_user,title_text='test watch party', start_date='2022-02-22', end_date='2022-02-23')
+        test_range = AvailabilityRange(account=my_user,watchparty=test_watchparty, start_time='2022-02-22 10:00:00', end_time='2022-02-22 12:00:00')
+        self.assertIs(test_range.valid_time_range(), True)
+    def test_invalid_time_range(self):
+        my_user = User.objects.create(username='Testuser')
+        test_watchparty = Watchparty(account=my_user,title_text='test watch party', start_date='2022-02-22', end_date='2022-02-23')
+        test_range = AvailabilityRange(account=my_user,watchparty=test_watchparty, start_time='2022-02-22 10:00:00', end_time='2022-02-22 09:00:00')
+        self.assertIs(test_range.valid_time_range(), False)
+    def test_before_start_range(self):
+        my_user = User.objects.create(username='Testuser')
+        test_watchparty = Watchparty(account=my_user,title_text='test watch party', start_date='2022-02-22', end_date='2022-02-23')
+        test_range = AvailabilityRange(account=my_user,watchparty=test_watchparty, start_time='2022-02-22 23:59:00', end_time='2022-02-22 09:00:00')
+        self.assertIs(test_range.valid_time_range(), False)
+    def test_after_end_range(self):
+        my_user = User.objects.create(username='Testuser')
+        test_watchparty = Watchparty(account=my_user,title_text='test watch party', start_date='2022-02-22', end_date='2022-02-23')
+        test_range = AvailabilityRange(account=my_user,watchparty=test_watchparty, start_time='2022-02-22 00:00:00', end_time='2022-02-23 00:00:00')
+        self.assertIs(test_range.valid_time_range(), False)
+
 
 
