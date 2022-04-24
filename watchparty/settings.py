1d48c9d01192366147f88f2bc94a9f073bd3bc9d
gs8ckb <gs8ckb@virginia.edu>
2022-04-24 15:34:09 -0400

    Clean Login Page

--- watchparty/settings.py
+++ watchparty/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'organizer',
     'django.contrib.sites',
-
+    'bootstrap5',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
