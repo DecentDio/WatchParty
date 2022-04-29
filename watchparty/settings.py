ffe3ac75f449ee9461323e0f37b27e1c216f8746
Shashank Balaji <sb5jg@virginia.edu>
2022-04-28 22:52:21 -0400

    Bootstrapped the favorites and check.html files

--- watchparty/settings.py
+++ watchparty/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-162*_fhrv$be1k*12tbquawrpd^$n7-lp-zw!f(y+sqy)wfp7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','b22-watch-party.herokuapp.com']
 
